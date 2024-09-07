13af4c24064c20ac372bfa44251a9b8440eef4db
Zephyr Skye <neon.shaman@gmail.com>
2024-09-06 17:17:11 -0700

    early error util package, some controller handling

--- controllers/users/users_controller.go
+++ controllers/users/users_controller.go
@@ -2,11 +2,27 @@ package users
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/neonshaman/bookstore_users-api/domain/users"
+	"github.com/neonshaman/bookstore_users-api/services"
+	"github.com/neonshaman/bookstore_users-api/utils/errors"
 	"net/http"
 )
 
 func CreateUser(c *gin.Context) {
-	c.String(http.StatusNotImplemented, "Implement mCreateUser!")
+	var user users.User
+
+	if err := c.ShouldBindJSON(&user); err != nil {
+		restErr := errors.NewBadRequestError("invalid json body")
+		c.JSON(restErr.Status, restErr)
+		return
+	}
+
+	result, saveErr := services.CreateUser(user)
+	if saveErr != nil {
+		c.JSON(saveErr.Status, saveErr)
+		return
+	}
+	c.JSON(http.StatusCreated, result)
 }
 
 func GetUser(c *gin.Context) {
--- services/users_service.go
+++ services/users_service.go
@@ -1,11 +1,17 @@
 package services
 
+import (
+	"github.com/neonshaman/bookstore_users-api/domain/users"
+	"github.com/neonshaman/bookstore_users-api/utils/errors"
+)
+
 func GetUser() {
 
 }
 
-func CreateUser() {
-
+func CreateUser(user users.User) (*users.User, *errors.RestErr) {
+	// TODO: Implement me!
+	return &user, nil
 }
 
 func FindUser() {
