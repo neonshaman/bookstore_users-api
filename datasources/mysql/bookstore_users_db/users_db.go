b1adf16465b42452828280d6494276450bc37434
Zephyr Skye <neon.shaman@gmail.com>
2024-09-10 14:55:47 -0700

    env stuff
    Please enter the commit message for your changes. Lines starting

--- datasources/mysql/bookstore_users_db/users_db.go
+++ datasources/mysql/bookstore_users_db/users_db.go
@@ -2,14 +2,30 @@ package bookstore_users_db
 
 import (
 	"database/sql"
+	"fmt"
+	_ "github.com/go-sql-driver/mysql"
+	"log"
+	"os"
+)
+
+const (
+	bookstore_users_username = "bookstore_users_username"
+	bookstore_users_password = "bookstore_users_password"
+	bookstore_users_host     = "bookstore_users_host"
+	bookstore_users_schema   = "bookstore_users_schema"
 )
 
 var (
 	Client *sql.DB
+	// grab from env for now, secrets managers of some kind later
+	username = os.Getenv(bookstore_users_username)
+	password = os.Getenv(bookstore_users_password)
+	host     = os.Getenv(bookstore_users_host)
+	schema   = os.Getenv(bookstore_users_schema)
 )
 
 func init() {
-	datasource := "root:root@tcp(127.0.01:3306)/bookstore_users"
+	datasource := fmt.Sprintf("%s:%s@tcp(%s)/%s", username, password, host, schema)
 	var err error // init error
 
 	Client, err = sql.Open("mysql", datasource)
@@ -20,4 +36,5 @@ func init() {
 	if err = Client.Ping(); err != nil {
 		panic(err)
 	}
+	log.Println("Successfully connected to MySQL")
 }
